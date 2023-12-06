97f314e7a26796293b6f0c4b4cab5f93dfdfd5f9
Anabel Villalobos <villalobos1998@gmail.com>
2023-12-05 23:01:19 -0800

    finished styling

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import "./home.css";
+import rosesLeft from "../../coverImage/rosesLeft.png"
+import rosesRight from "../../coverImage/rosesRight.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 
 export default function Home() {
 
   return (
     <div className="home card">
-      <div className="card-body items-center text-center about-section">
-        <h1 className="text-3xl card-title">Anthony and Anabel</h1>
-        <h1 className="text-xl card-title">May 26, 2024</h1>
-        <p>Temecula, CA</p>
+      <div className="flex items-start sm:items-center">
+        <img className="w-20 sm:w-1/5" src={rosesLeft} />
+        <div className="home-header card-body flex flex-col items-center about-section sm:p-0 px-0">
+          <h1 className="sm:text-3xl text-center font-bold">Anthony and Anabel</h1>
+          <h1 className="text-sm sm:text-xl">May 26, 2024</h1>
+          <div className="flex items-center gap-2">
+            <FontAwesomeIcon icon={faMapMarkerAlt} />
+            <p className="text-sm sm:text-xl">Temecula, CA</p>
+          </div>
+        </div>
+        <img className="w-20 sm:w-1/5" src={rosesRight} />
       </div>
       <figure>
         <img
-          src="/coverImage/cover.jpg"
+          src="../../coverImage/cover.jpg"
           alt="Engagement day"
         />
       </figure>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { HashRouter  } from "react-router-dom";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
 
 import "./index.css";
 import App from "./App";
 
+library.add(fas);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
