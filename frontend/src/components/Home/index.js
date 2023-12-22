50d7c27c858865cca0ba4ff1e9394b85573ba7bd
Anabel Villalobos <villalobos1998@gmail.com>
2023-12-21 21:29:31 -0800

    added engagement photos and changed cover photo

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -5,16 +5,16 @@ import "./home.css";
 export default function Home() {
 
   return (
-    <div className="home card">
+    <div className="home card justify-center items-center">
       <div className="card-body items-center text-center about-section">
         <h1 className="text-3xl card-title">Anthony and Anabel</h1>
         <h1 className="text-xl card-title">May 26, 2024</h1>
         <p>Temecula, CA</p>
       </div>
-      <figure>
+      <figure className="lg:w-1/2 sm:w-3/4">
         <img
           src="/coverImage/cover.jpg"
-          alt="Engagement day"
+          alt="Cover photo"
         />
       </figure>
     </div>
